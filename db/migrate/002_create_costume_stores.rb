3c74e1768dcdba25b9b062f9f6afa180cc4abc6a
Caroline Longnecker <cvrieous@gmail.com>
2020-06-15 03:41:11 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :employee_count
+      t.integer :employees
       t.boolean :in_business
       t.datetime :opening_time
       t.datetime :closing_time
