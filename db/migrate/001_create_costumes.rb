aee63573b0bebab2eac8ea65d301c7dfdbc0629e
Caroline Longnecker <cvrieous@gmail.com>
2020-06-15 03:28:37 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,12 +10,12 @@
 class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
-      t.string name
-      t.float price
-      t.string size
-      t.string image_url
-      t.timestamp created_at
-      t.timestamp updated_at
+      t.string :name
+      t.float :price
+      t.string :size
+      t.string :image_url
+      t.timestamp :created_at
+      t.timestamp :updated_at
     end
   end
 end
